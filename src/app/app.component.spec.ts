cf8a16d609b4998692ee58e20daf1018d0b039de
kirestein <erik.proenca2011@gmail.com>
2021-09-08 09:48:24 -0300

    Configurando os testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,19 @@
+import { CalculadoraModule } from './calculadora/calculadora.module';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
-      ],
+        RouterTestingModule,
+        CalculadoraModule
+        ],
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
   });
 
